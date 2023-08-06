3d6f25ce044c67a97f0fbe7ecdf0cc89e98b6c75
joao-arthur <joao.lothamer@gmail.com>
2023-08-06 07:44:01 -0300

    refactor(frontend): calendar header

--- frontend/src/content/calendar/table/display/Display.tsx
+++ frontend/src/content/calendar/table/display/Display.tsx
@@ -3,36 +3,74 @@ import { calendarFns } from "frontend_core";
 import { useCalendar } from "@/features/calendar/useCalendar";
 import { useCalendarEvent } from "@/features/calendarEvent/useCalendarEvent";
 import { Cell } from "./Cell";
+import { useDevice } from "@/lib/device/useDevice";
+import { Text } from "@/components/atoms/Text";
+
+const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+
+const daysOfWeekAbbrev = [
+    "Sun",
+    "Mon",
+    "Tue",
+    "Wed",
+    "Thu",
+    "Fri",
+    "Sat",
+];
+
+// unify both headers
 
 export function Display() {
+    const isMobile = useDevice().isMobile();
     const { year, month } = useCalendar();
     const calendar = { year, month };
     const { getDateEvents } = useCalendarEvent();
     const { height } = useWindowSize();
+    const weekDays = isMobile ? daysOfWeekAbbrev : daysOfWeek;
 
     return (
-        <div className="w-full h-full" style={{ height: `calc(${height}px - 5.25rem)` }}>
+        <>
             <div
-                className="grid w-full h-full"
-                style={{ gridTemplateRows: "repeat(6, 1fr)" }}
+                className="grid h-6"
+                style={{ gridTemplateColumns: "repeat(7, 1fr)" }}
             >
-                {calendarFns.toTable(calendar).map((week, weekIndex) => (
-                    <div
-                        className="grid min-h-0"
-                        style={{ gridTemplateColumns: "repeat(7, 1fr)" }}
-                        key={weekIndex}
-                    >
-                        {week.map((date) => (
-                            <Cell
-                                calendar={calendar}
-                                date={date}
-                                key={date}
-                                events={getDateEvents(date)}
-                            />
-                        ))}
-                    </div>
+                {daysOfWeek.map((weekDay) => (
+                    <Text key={weekDay}>
+                        <div className="select-none text-center font-bold m-auto">{weekDay}</div>
+                    </Text>
                 ))}
             </div>
-        </div>
+            <div className="w-full h-full" style={{ height: `calc(${height}px - 6.75rem)` }}>
+                <div
+                    className="grid w-full h-full"
+                    style={{ gridTemplateRows: "repeat(6, 1fr)" }}
+                >
+                    {calendarFns.toTable(calendar).map((week, weekIndex) => (
+                        <div
+                            className="grid min-h-0"
+                            style={{ gridTemplateColumns: "repeat(7, 1fr)" }}
+                            key={weekIndex}
+                        >
+                            {week.map((date) => (
+                                <Cell
+                                    calendar={calendar}
+                                    date={date}
+                                    key={date}
+                                    events={getDateEvents(date)}
+                                />
+                            ))}
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
     );
 }
