a8693e84c6a166bd45fda63c5a829acdacee8d5e
joao-arthur <joao.lothamer@gmail.com>
2023-08-06 21:24:21 -0300

    feat(frontend): delete charts and settings

---
+++ frontend/src/content/base/Header.tsx
@@ -0,0 +1,66 @@
+import type { ReactNode } from "react";
+import { useRouter } from "next/router";
+import { cl } from "@/lib/cl";
+import { useTheme } from "@/features/theme/useTheme";
+import { useSession } from "@/features/session/useSession";
+import { useSessionManager } from "@/features/session/useSessionManager";
+import { Toggle } from "@/components/atoms/input/Toggle";
+import { HoverButton } from "@/components/atoms/extraButton/HoverButton";
+import { Link } from "@/components/atoms/Link";
+import { If } from "@/components/atoms/layout/If";
+
+type props = {
+    readonly left: ReactNode;
+};
+
+export function Header({ left }: props) {
+    const logged = useSession().logged();
+    const [theme, setTheme] = useTheme();
+    const { pathname } = useRouter();
+    const { unlog } = useSessionManager();
+
+    return (
+        <header
+            className={cl(
+                "flex w-full flex-shrink-0 justify-between px-2 h-12",
+                "bg-primary dark:bg-primary-dark",
+                "transition-colors duration-300",
+            )}
+        >
+            <div className="flex items-center gap-3">
+                {left}
+            </div>
+            <div className="flex items-center gap-3">
+                <Toggle
+                    value={theme === "dark"}
+                    onChange={setTheme}
+                    display={{ on: "🌑", off: "☀️" }}
+                />
+                {logged
+                    ? (
+                        <HoverButton onClick={() => unlog()}>
+                            SIGN IN
+                        </HoverButton>
+                    )
+                    : (
+                        <div>
+                            <If condition={pathname !== "/signin"}>
+                                <Link to="/signin">
+                                    <HoverButton>
+                                        SIGN IN
+                                    </HoverButton>
+                                </Link>
+                            </If>
+                            <If condition={pathname !== "/signup"}>
+                                <Link to="/signup">
+                                    <HoverButton>
+                                        SIGN UP
+                                    </HoverButton>
+                                </Link>
+                            </If>
+                        </div>
+                    )}
+            </div>
+        </header>
+    );
+}
--- frontend/src/content/charts/EventsTable/DisabledColumn.tsx
+++
@@ -1,5 +0,0 @@
-export function DisabledColumn() {
-    return (
-        <th className="border border-gray-300 dark:border-gray-500 bg-gray-100 transition-colors duration-300 dark:bg-dark" />
-    );
-}
