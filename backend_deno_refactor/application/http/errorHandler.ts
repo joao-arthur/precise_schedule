cb9873ad6b8003cf3f67cfcd128314784e78ab81
joao-arthur <joao.lothamer@gmail.com>
2024-06-19 22:04:07 -0300

    fix: run

--- backend_deno_refactor/application/http/errorHandler.ts
+++ backend_deno_refactor/application/http/errorHandler.ts
@@ -1,6 +1,7 @@
 import type { HTTPResponse } from "./response.ts";
 import { ValidationError } from "../../domain/validation/validate.ts";
 import { SessionDecodeError } from "../../domain/session/service.ts";
+import { BusinessError } from "../../domain/general.ts";
 import { badRequest, internalServerError, unauthorized } from "./response.ts";
 
 export function errorHandler(error: Error): HTTPResponse {
