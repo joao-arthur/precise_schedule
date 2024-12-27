0408103f064277c3dade4b0456af06f21787b4cb
joao-arthur <joao.lothamer@gmail.com>
2024-12-27 13:14:07 -0300

    feat(backend): error and success response localized

--- backend/src/entry/user_controller.rs
+++ backend/src/entry/user_controller.rs
@@ -9,8 +9,9 @@ use rocket::serde::{Deserialize, Serialize};
 use rocket::{http::Status, post, response::status, Data};
 use serde_json::Value;
 
+use crate::domain::schedule::user::create::{user_c, UserC};
 use crate::domain::validation::{Schema, V};
-use crate::entry::deps::get_validator;
+use crate::entry::deps::{get_validator, get_date_time_gen, get_id_gen, get_session_service, get_user_repo};
 use crate::infra::validation::adapter::{
     validation_i18n, value_from_json_value
 };
@@ -59,15 +60,26 @@ struct ErrorGeneric {
 }
 
 #[post("/", format = "application/json", data = "<data>")]
-pub async fn endpoint_user_c(data: Data<'_>, lg: LanguageGuard) -> Custom<content::RawJson<String>> {
-    let limit = 1.mebibytes();
-    let body = match data.open(limit).into_string().await {
-        Ok(body) => body.value,
+pub async fn endpoint_user_c(
+    data: Data<'_>,
+    lg: LanguageGuard
+) -> Result<Json<UserCResult>, Custom<String>> {
+    let limit = 1.kilobytes();
+    let body = match data.open(limit).into_bytes().await {
+        Ok(body) => {
+            if body.len() >= limit {
+                return Err(status::Custom(
+                    Status::PayloadTooLarge,
+                    serde_json::to_string(&ErrorGeneric { error: "The payload is too large".to_owned()}).unwrap(),
+                ));
+            }
+            body.value
+        },
         Err(_) => {
-            return status::Custom(
+            return Err(status::Custom(
                 Status::PayloadTooLarge,
-                Json(ErrorGeneric { error: "The payload is too large".to_owned()}),
-            );
+                serde_json::to_string(&ErrorGeneric { error: "The payload is too large".to_owned()}).unwrap(),
+            ));
         }
     };
     static USER_C_SCHEMA: LazyLock<Schema> = LazyLock::new(|| {
@@ -76,91 +88,57 @@ pub async fn endpoint_user_c(data: Data<'_>, lg: LanguageGuard) -> Custom<conten
             ("birthdate", vec![V::Required, V::Str, V::Dt, V::DtMin("1970-01-01")]),
             ("email", vec![V::Required, V::Str, V::Email]),
             ("username", vec![V::Required, V::Str, V::StrMinLen(1), V::StrMaxLen(32)]),
-            (
-                "password",
-                vec![
-                    V::Required,
-                    V::Str,
-                    V::StrMinLen(1),
-                    V::StrMaxLen(32),
-                    V::StrMinUpper(1),
-                    V::StrMinLower(1),
-                    V::StrMinSpecial(1),
-                    V::StrMinNum(1),
-                ],
-            ),
+            ("password", vec![V::Required, V::Str, V::StrMinLen(1), V::StrMaxLen(32), V::StrMinUpper(1), V::StrMinLower(1), V::StrMinSpecial(1), V::StrMinNum(1)]),
         ])
     });
-    let json_value: Value = serde_json::from_str(&body).unwrap();
+    let json_value: Value = serde_json::from_slice(&body).unwrap();
     let internal_value = value_from_json_value(json_value);
     let res = get_validator().validate(&USER_C_SCHEMA, &internal_value);
     match res {
         Ok(_data) => {
-
-
-
-            return status::Custom(Status::Ok, content::RawJson("".to_owned()));
+            let ff: UserCCDD = serde_json::from_slice(&body).unwrap();
+            let user = UserC {
+                email: ff.email.to_string(),
+                first_name: ff.first_name.to_string(),
+                birthdate: ff.birthdate.to_string(),
+                username: ff.username.to_string(),
+                password: ff.password.to_string(),
+            };
+            let repo = get_user_repo();
+            let temp = user_c(
+                get_validator(),
+                repo,
+                get_id_gen(),
+                get_date_time_gen(),
+                get_session_service(),
+                user,
+            )
+            .unwrap();
+            return Ok(Json(UserCResult {
+                user: User {
+                    id: temp.user.id.clone(),
+                    first_name: temp.user.first_name.clone(),
+                    birthdate: temp.user.birthdate.clone(),
+                    email: temp.user.email.clone(),
+                    username: temp.user.username.clone(),
+                    created_at: temp.user.created_at.clone(),
+                    password: temp.user.password.clone(),
+                    updated_at: temp.user.updated_at.clone(),
+                },
+                session: Session { token: temp.session.token },
+            }));
         }
         Err(err) => {
             let erri18n: HashMap<&str, Vec<String>> = err
                 .into_iter()
                 .map(|f| (f.0, f.1.iter().map(|p|validation_i18n(p, &lg.0)).collect::<Vec<String>>()))
                 .collect();
-            return status::Custom(
+            return Err(status::Custom(
                 Status::UnprocessableEntity,
-                content::RawJson(serde_json::to_string(&erri18n).unwrap()),
-            );
+                serde_json::to_string(&erri18n).unwrap(),
+            ));
         }
     }
-
-    // Attempt to deserialize the JSON
-    // let task: Result<User, _> = json::from_str(&body);
-    // match task {
-    //     Ok(task) => status::Custom(
-    //         Status::Ok,
-    //         format!(
-    //             "Task received: description = {}, complete = {}",
-    //             task.email, task.first_name
-    //         ),
-    //     ),
-    //     Err(err) => status::Custom(
-    //         Status::UnprocessableEntity,
-    //         format!("Failed to parse JSON: {}", err),
-    //     ),
-    // }
-
-    /* let user = UserC {
-        email: user.email.to_string(),
-        first_name: user.first_name.to_string(),
-        birthdate: user.birthdate.to_string(),
-        username: user.username.to_string(),
-        password: user.password.to_string(),
-    };
-    let repo = get_user_repo();
-
-    let temp = user_c(
-        get_validator(),
-        repo,
-        get_id_gen(),
-        get_date_time_gen(),
-        get_session_service(),
-        user,
-    )
-    .unwrap();
-
-    Json(UserCResult {
-        user: User {
-            id: temp.user.id.clone(),
-            first_name: temp.user.first_name.clone(),
-            birthdate: temp.user.birthdate.clone(),
-            email: temp.user.email.clone(),
-            username: temp.user.username.clone(),
-            created_at: temp.user.created_at.clone(),
-            password: temp.user.password.clone(),
-            updated_at: temp.user.updated_at.clone(),
-        },
-        session: Session { token: temp.session.token },
-    }) */
 }
 
 #[put("/", format = "application/json")]
