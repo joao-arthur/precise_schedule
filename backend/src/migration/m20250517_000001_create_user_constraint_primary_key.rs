b29ea45a202b9a2c7dde383d4662299c55dc67cb
joao-arthur <joao.lothamer@gmail.com>
2025-05-17 12:14:03 -0300

    feat(backend): pk migration

---
+++ backend/src/migration/m20250517_000001_create_user_constraint_primary_key.rs
@@ -0,0 +1,18 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let db = manager.get_connection();
+        db.execute_unprepared(r#"alter table "user" add constraint user_pk primary key (id)"#).await?;
+        Ok(())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager.get_connection().execute_unprepared(r#"drop constraint if exists user_pk"#).await?;
+        Ok(())
+    }
+}
--- backend/src/migration/mod.rs
+++ backend/src/migration/mod.rs
@@ -1,12 +1,16 @@
 use sea_orm_migration::prelude::*;
 
-mod m20250516_000001_create_post_table;
+mod m20250516_000001_create_user_table;
+mod m20250517_000001_create_user_constraint_primary_key;
 
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(m20250516_000001_create_post_table::Migration)]
+        vec![
+            Box::new(m20250516_000001_create_user_table::Migration),
+            Box::new(m20250517_000001_create_user_constraint_primary_key::Migration),
+        ]
     }
 }
