db40b818a432783bdebd0dd305658be0c7860174
joao-arthur <joao.lothamer@gmail.com>
2025-05-18 20:02:25 -0300

    feat(backend): user.first_name not null migration

--- backend/src/migration/mod.rs
+++ backend/src/migration/mod.rs
@@ -15,6 +15,7 @@ mod m20250518_12_create_app_user_email_column;
 mod m20250518_13_create_app_user_email_not_null;
 mod m20250518_14_create_app_user_email_unique;
 mod m20250518_15_create_app_user_first_name_column;
+mod m20250518_16_create_app_user_first_name_not_null;
 
 pub struct Migrator;
 
@@ -37,6 +38,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20250518_13_create_app_user_email_not_null::Migration),
             Box::new(m20250518_14_create_app_user_email_unique::Migration),
             Box::new(m20250518_15_create_app_user_first_name_column::Migration),
+            Box::new(m20250518_16_create_app_user_first_name_not_null::Migration),
         ]
     }
 }
