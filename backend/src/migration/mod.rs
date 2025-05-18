c300d82695a9821998c19e06df9a4c1a3ddaba0b
joao-arthur <joao.lothamer@gmail.com>
2025-05-18 19:59:46 -0300

    feat(backend): user.email unique migration

--- backend/src/migration/mod.rs
+++ backend/src/migration/mod.rs
@@ -13,6 +13,7 @@ mod m20250518_10_create_app_user_password_column;
 mod m20250518_11_create_app_user_password_not_null;
 mod m20250518_12_create_app_user_email_column;
 mod m20250518_13_create_app_user_email_not_null;
+mod m20250518_14_create_app_user_email_unique;
 
 pub struct Migrator;
 
@@ -33,6 +34,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20250518_11_create_app_user_password_not_null::Migration),
             Box::new(m20250518_12_create_app_user_email_column::Migration),
             Box::new(m20250518_13_create_app_user_email_not_null::Migration),
+            Box::new(m20250518_14_create_app_user_email_unique::Migration),
         ]
     }
 }
