aaf8148404cd6f3f813fca625be8c782687538a6
joao-arthur <joao.lothamer@gmail.com>
2025-05-19 19:01:52 -0300

    feat(backend): DBRepository implementation

--- backend/src/infra/schedule/user/db_repository.rs
+++ backend/src/infra/schedule/user/db_repository.rs
@@ -1,6 +1,6 @@
 use super::entity;
 use domain::{
-    database::{DBErr, DBOp},
+    database::DBErr,
     schedule::user::{
         model::{User, UserCredentials},
         repository::UserRepository,
@@ -9,52 +9,6 @@ use domain::{
 };
 use sea_orm::*;
 
-pub struct Mutation;
-
-impl Mutation {
-    pub async fn create_user(
-        db: &DbConn,
-        form_data: entity::Model,
-    ) -> Result<entity::ActiveModel, DbErr> {
-        entity::ActiveModel { id: Set(form_data.id.to_owned()) }.save(db).await
-    }
-
-    pub async fn update_user(
-        db: &DbConn,
-        form_data: entity::Model,
-    ) -> Result<entity::Model, DbErr> {
-        entity::ActiveModel { id: Set(form_data.id.to_owned()) }.update(db).await
-    }
-
-    pub async fn delete_user(db: &DbConn, id: String) -> Result<DeleteResult, DbErr> {
-        let user: entity::ActiveModel = entity::Entity::find_by_id(id)
-            .one(db)
-            .await?
-            .ok_or(DbErr::Custom("Cannot find user.".to_owned()))
-            .map(Into::into)?;
-        user.delete(db).await
-    }
-}
-
-pub struct Query;
-
-impl Query {
-    pub async fn read_user_by_id(db: &DbConn, id: String) -> Result<Option<entity::Model>, DbErr> {
-        entity::Entity::find_by_id(id).one(db).await
-    }
-
-    pub async fn find_users_in_page(
-        db: &DbConn,
-        page: u64,
-        users_per_page: u64,
-    ) -> Result<(Vec<entity::Model>, u64), DbErr> {
-        let paginator =
-            entity::Entity::find().order_by_asc(entity::Column::Id).paginate(db, users_per_page);
-        let num_pages = paginator.num_pages().await?;
-        paginator.fetch_page(page - 1).await.map(|p| (p, num_pages))
-    }
-}
-
 pub struct UserRepositoryDB<'a> {
     pub db: &'a DbConn,
 }
@@ -62,40 +16,142 @@ pub struct UserRepositoryDB<'a> {
 unsafe impl<'a> Sync for UserRepositoryDB<'a> {}
 
 impl<'a> UserRepository for UserRepositoryDB<'a> {
-    async fn create(&self, user: &User) -> DBOp<()> {
-        let form_data = entity::Model { id: user.id.clone() };
-        let res = Mutation::create_user(self.db, form_data).await;
+    async fn create(&self, user: &User) -> Result<(), DBErr> {
+        let id = uuid::Uuid::try_parse(&user.id).map_err(|_| DBErr)?;
+        let birthdate = chrono::NaiveDate::parse_from_str(&user.birthdate, "%Y-%m-%d").map_err(|_| DBErr)?;
+        let created_at =
+            chrono::DateTime::parse_from_rfc3339(&user.created_at).map_err(|_| DBErr)?;
+        let updated_at =
+            chrono::DateTime::parse_from_rfc3339(&user.updated_at).map_err(|_| DBErr)?;
+        let model = entity::Model {
+            id: id.clone(),
+            username: user.username.clone(),
+            password: user.password.clone(),
+            email: user.email.clone(),
+            first_name: user.first_name.clone(),
+            birthdate: birthdate.clone(),
+            created_at: created_at.clone(),
+            updated_at: updated_at.clone(),
+        };
+        let active_model = entity::ActiveModel {
+            id: Set(model.id.to_owned()),
+            username: Set(model.username.to_owned()),
+            password: Set(model.password.to_owned()),
+            email: Set(model.email.to_owned()),
+            first_name: Set(model.first_name.to_owned()),
+            birthdate: Set(model.birthdate.to_owned()),
+            created_at: Set(model.created_at.to_owned()),
+            updated_at: Set(model.updated_at.to_owned()),
+        };
+        let res = active_model.save(self.db).await;
         match res {
             Ok(_) => Ok(()),
             Err(_) => Err(DBErr),
         }
     }
 
-    async fn update(&self, user: &User) -> DBOp<()> {
-        let form_data = entity::Model { id: user.id.clone() };
-        let res = Mutation::update_user(self.db, form_data).await;
+    async fn update(&self, user: &User) -> Result<(), DBErr> {
+        let id = uuid::Uuid::try_parse(&user.id).map_err(|_| DBErr)?;
+        let birthdate = chrono::NaiveDate::parse_from_str(&user.birthdate, "%Y-%m-%d").map_err(|_| DBErr)?;
+        let created_at =
+            chrono::DateTime::parse_from_rfc3339(&user.created_at).map_err(|_| DBErr)?;
+        let updated_at =
+            chrono::DateTime::parse_from_rfc3339(&user.updated_at).map_err(|_| DBErr)?;
+        let model = entity::Model {
+            id: id.clone(),
+            username: user.username.clone(),
+            password: user.password.clone(),
+            email: user.email.clone(),
+            first_name: user.first_name.clone(),
+            birthdate: birthdate.clone(),
+            created_at: created_at.clone(),
+            updated_at: updated_at.clone(),
+        };
+        let active_model = entity::ActiveModel::from(model);
+        let res = active_model.update(self.db).await;
         match res {
             Ok(_) => Ok(()),
             Err(_) => Err(DBErr),
         }
     }
 
-    async fn delete(&self, id: &str) -> DBOp<()> {
-        Ok(())
+    async fn delete(&self, id: &str) -> Result<(), DBErr> {
+        let id = uuid::Uuid::try_parse(id).map_err(|_| DBErr)?;
+        let model =
+            entity::Entity::find_by_id(id).one(self.db).await.map_err(|_| DBErr)?.ok_or(DBErr)?;
+        let active_model = entity::ActiveModel::from(model);
+        let res = active_model.delete(self.db).await;
+        match res {
+            Ok(_) => Ok(()),
+            Err(_) => Err(DBErr),
+        }
     }
 
-    async fn read_by_credentials(&self, credentials: &UserCredentials) -> DBOp<Option<User>> {
-        Ok(None)
+    async fn read_by_credentials(
+        &self,
+        credentials: &UserCredentials,
+    ) -> Result<Option<User>, DBErr> {
+        let res = entity::Entity::find()
+            .filter(
+                entity::Column::Username
+                    .eq(credentials.username.clone())
+                    .and(entity::Column::Password.eq(credentials.password.clone())),
+            )
+            .one(self.db)
+            .await;
+        match res {
+            Ok(value) => match value {
+                Some(user) => Ok(Some(User {
+                    id: user.id.to_string(),
+                    username: user.username.clone(),
+                    password: user.password.clone(),
+                    email: user.email.clone(),
+                    first_name: user.first_name.clone(),
+                    birthdate: user.birthdate.format("%Y-%m-%d").to_string(),
+                    created_at: user.created_at.to_rfc3339(),
+                    updated_at: user.updated_at.to_rfc3339(),
+                })),
+                None => Ok(None),
+            },
+            Err(_) => Err(DBErr),
+        }
     }
 
-    async fn read_by_id(&self, id: &str) -> DBOp<Option<User>> {
-        Ok(None)
+    async fn read_by_id(&self, id: &str) -> Result<Option<User>, DBErr> {
+        let id = uuid::Uuid::try_parse(id).map_err(|_| DBErr)?;
+        let res = entity::Entity::find_by_id(id).one(self.db).await;
+        match res {
+            Ok(value) => match value {
+                Some(user) => Ok(Some(User {
+                    id: user.id.to_string(),
+                    username: user.username.clone(),
+                    password: user.password.clone(),
+                    email: user.email.clone(),
+                    first_name: user.first_name.clone(),
+                    birthdate: user.birthdate.format("%Y-%m-%d").to_string(),
+                    created_at: user.created_at.to_rfc3339(),
+                    updated_at: user.updated_at.to_rfc3339(),
+                })),
+                None => Ok(None),
+            },
+            Err(_) => Err(DBErr),
+        }
     }
 
     async fn read_count_unique_info(
         &self,
         user_unique_info: &UserUniqueInfo,
-    ) -> DBOp<UserUniqueInfoCount> {
-        Ok(UserUniqueInfoCount { email: 0, username: 0 })
+    ) -> Result<UserUniqueInfoCount, DBErr> {
+        let res_username = entity::Entity::find()
+            .filter(entity::Column::Username.eq(user_unique_info.username.clone()))
+            .count(self.db)
+            .await
+            .map_err(|_|DBErr)?;
+        let res_email = entity::Entity::find()
+            .filter(entity::Column::Email.eq(user_unique_info.email.clone()))
+            .count(self.db)
+            .await
+            .map_err(|_|DBErr)?;
+        Ok(UserUniqueInfoCount { email: res_email as u32, username: res_username as u32 })
     }
 }
--- backend/src/infra/schedule/user/entity.rs
+++ backend/src/infra/schedule/user/entity.rs
@@ -1,12 +1,28 @@
+use chrono::{FixedOffset, Utc};
 use sea_orm::entity::prelude::*;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel, Serialize, Deserialize)]
-#[sea_orm(table_name = "user")]
+#[sea_orm(table_name = "app_user")]
 pub struct Model {
-    #[sea_orm(primary_key, column_type = "Text")]
-    #[serde(skip_deserializing)]
-    pub id: String,
+    #[sea_orm(primary_key)]
+    pub id: uuid::Uuid,
+
+    pub username: String,
+
+    pub password: String,
+
+    #[sea_orm(unique)]
+    pub email: String,
+
+    pub first_name: String,
+
+    #[sea_orm(unique)]
+    pub birthdate: chrono::NaiveDate,
+
+    pub created_at: chrono::DateTime<FixedOffset>,
+
+    pub updated_at: chrono::DateTime<FixedOffset>,
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
