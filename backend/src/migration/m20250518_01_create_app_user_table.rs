9591be02668fdb58e3b156620b819993ada0b315
joao-arthur <joao.lothamer@gmail.com>
2025-05-19 18:55:33 -0300

    refactor(backend): app_user id as uuid

--- backend/src/migration/m20250518_01_create_app_user_table.rs
+++ backend/src/migration/m20250518_01_create_app_user_table.rs
@@ -7,7 +7,7 @@ pub struct Migration;
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         let db = manager.get_connection();
-        db.execute_unprepared("CREATE TABLE app_user (id VARCHAR(36) NOT NULL)").await?;
+        db.execute_unprepared("CREATE TABLE app_user (id UUID)").await?;
         Ok(())
     }
 
