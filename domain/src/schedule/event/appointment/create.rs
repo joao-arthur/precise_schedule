eea92cd3889a557ef901ec2a7f2e381a648bb657
joao-arthur <joao.lothamer@gmail.com>
2025-05-22 19:03:37 -0300

    test: event_appointment_create

--- domain/src/schedule/event/appointment/create.rs
+++ domain/src/schedule/event/appointment/create.rs
@@ -14,7 +14,7 @@ use crate::{
     },
 };
 
-pub struct AppointmentCreate {
+pub struct AppointmentCreateInput {
     pub name: String,
     pub day: String,
     pub begin: String,
@@ -37,7 +37,7 @@ pub static APPOINTMENT_CREATE_SCHEMA: LazyLock<Schema> = LazyLock::new(|| {
     ]))
 });
 
-pub fn transform_to_event_create(model: AppointmentCreate) -> EventCreateInput {
+pub fn transform_to_event_create(model: AppointmentCreateInput) -> EventCreateInput {
     EventCreateInput {
         name: model.name,
         begin: format!("{}T{}Z", model.day, model.begin),
@@ -56,7 +56,7 @@ pub async fn event_appointment_create<
     repository: &Repo,
     id_generator: &IdGen,
     date_time_generator: &DtTmGen,
-    model: AppointmentCreate,
+    model: AppointmentCreateInput,
     user_id: String,
 ) -> Result<Event, EventErr> {
     let event_create_model = transform_to_event_create(model);
@@ -65,17 +65,17 @@ pub async fn event_appointment_create<
 
 #[cfg(test)]
 mod tests {
-    use crate::schedule::event::{
+    use crate::{generator::stub::{DateTimeGeneratorStub, IdGeneratorStub}, schedule::event::{
         create::EventCreateInput,
-        model::{EventCategory, EventFrequency},
-    };
+        model::{Event, EventCategory, EventFrequency}, repository::stub::EventRepositoryStub,
+    }};
 
-    use super::{AppointmentCreate, transform_to_event_create};
+    use super::{AppointmentCreateInput, transform_to_event_create, event_appointment_create};
 
     #[test]
     fn test_transform_to_event_create() {
         assert_eq!(
-            transform_to_event_create(AppointmentCreate {
+            transform_to_event_create(AppointmentCreateInput {
                 name: "Dentist".into(),
                 day: "2024-03-31".into(),
                 begin: "18:00".into(),
@@ -91,6 +91,39 @@ mod tests {
                 frequency: Some(EventFrequency::D2),
                 weekend_repeat: Some(true),
             }
+        );
+    }
+
+    #[tokio::test]
+    async fn event_appointment_create_ok() {
+        assert_eq!(
+            event_appointment_create(
+                &EventRepositoryStub::of_empty(),
+                &IdGeneratorStub("6d470410-5e51-40d1-bd13-0bb6a99de95e".into()),
+                &DateTimeGeneratorStub::of_iso("2025-02-05T22:49Z".into()),
+                AppointmentCreateInput {
+                    name: "Dentist".into(),
+                    day: "2024-03-31".into(),
+                    begin: "18:00".into(),
+                    end: "22:00".into(),
+                    frequency: Some(EventFrequency::D2),
+                    weekend_repeat: Some(true),
+                },
+                "a6edc906-2f9f-5fb2-a373-efac406f0ef2".into()
+            )
+            .await,
+            Ok(Event {
+                id: "6d470410-5e51-40d1-bd13-0bb6a99de95e".into(),
+                name: "Dentist".into(),
+                begin: "2024-03-31T18:00Z".into(),
+                end: "2024-03-31T22:00Z".into(),
+                category: EventCategory::Appointment,
+                frequency: Some(EventFrequency::D2),
+                weekend_repeat: Some(true),
+                user: "a6edc906-2f9f-5fb2-a373-efac406f0ef2".into(),
+                created_at: "2025-02-05T22:49Z".into(),
+                updated_at: "2025-02-05T22:49Z".into(),
+            })
         );
     }
 }
