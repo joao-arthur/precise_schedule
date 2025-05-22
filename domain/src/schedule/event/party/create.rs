e9ad3f4887071183555b75b871432640c1d012f7
joao-arthur <joao.lothamer@gmail.com>
2025-05-22 20:46:43 -0300

    feat(domain): event party create

--- domain/src/schedule/event/party/create.rs
+++ domain/src/schedule/event/party/create.rs
@@ -2,14 +2,24 @@ use std::sync::LazyLock;
 
 use araucaria::schema::{DateSchema, ObjSchema, Schema, StrSchema, TimeSchema};
 
-pub struct PartyCreate {
+use crate::{
+    generator::{DateTimeGenerator, IdGenerator},
+    schedule::event::{
+        create::{EventCreateInput, event_create},
+        error::EventErr,
+        model::{Event, EventCategory},
+        repository::EventRepository,
+    },
+};
+
+pub struct PartyCreateInput {
     pub name: String,
     pub day: String,
     pub begin: String,
     pub end: String,
 }
 
-pub static PARTY_CREATE_SCHEMA: LazyLock<Schema> = LazyLock::new(|| {
+pub static DATE_CREATE_SCHEMA: LazyLock<Schema> = LazyLock::new(|| {
     Schema::from(ObjSchema::from([
         ("name".into(), Schema::from(StrSchema::default().chars_len_btwn(1, 32))),
         ("day".into(), Schema::from(DateSchema::default().unix_epoch())),
@@ -18,4 +28,93 @@ pub static PARTY_CREATE_SCHEMA: LazyLock<Schema> = LazyLock::new(|| {
     ]))
 });
 
-pub async fn event_party_create() {}
+pub fn transform_to_event_create(model: PartyCreateInput) -> EventCreateInput {
+    EventCreateInput {
+        name: model.name,
+        begin: format!("{}T{}Z", model.day, model.begin),
+        end: format!("{}T{}Z", model.day, model.end),
+        category: EventCategory::Party,
+        frequency: None,
+        weekend_repeat: None,
+    }
+}
+
+pub async fn event_party_create<
+    Repo: EventRepository,
+    IdGen: IdGenerator,
+    DtTmGen: DateTimeGenerator,
+>(
+    repository: &Repo,
+    id_generator: &IdGen,
+    party_time_generator: &DtTmGen,
+    model: PartyCreateInput,
+    user_id: String,
+) -> Result<Event, EventErr> {
+    let event_create_model = transform_to_event_create(model);
+    event_create(repository, id_generator, party_time_generator, event_create_model, user_id).await
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        generator::stub::{DateTimeGeneratorStub, IdGeneratorStub},
+        schedule::event::{
+            create::EventCreateInput,
+            model::{Event, EventCategory},
+            repository::stub::EventRepositoryStub,
+        },
+    };
+
+    use super::{PartyCreateInput, event_party_create, transform_to_event_create};
+
+    #[test]
+    fn test_transform_to_event_create() {
+        assert_eq!(
+            transform_to_event_create(PartyCreateInput {
+                name: "Company year celebration".into(),
+                day: "2025-11-29".into(),
+                begin: "18:30".into(),
+                end: "23:59".into(),
+            }),
+            EventCreateInput {
+                name: "Company year celebration".into(),
+                begin: "2025-11-29T18:30Z".into(),
+                end: "2025-11-29T23:59Z".into(),
+                category: EventCategory::Party,
+                frequency: None,
+                weekend_repeat: None,
+            }
+        );
+    }
+
+    #[tokio::test]
+    async fn event_party_create_ok() {
+        assert_eq!(
+            event_party_create(
+                &EventRepositoryStub::of_empty(),
+                &IdGeneratorStub("6d470410-5e51-40d1-bd13-0bb6a99de95e".into()),
+                &DateTimeGeneratorStub::of_iso("2025-02-05T22:49Z".into()),
+                PartyCreateInput {
+                    name: "Company year celebration".into(),
+                    day: "2025-11-29".into(),
+                    begin: "18:30".into(),
+                    end: "23:59".into(),
+                },
+                "a6edc906-2f9f-5fb2-a373-efac406f0ef2".into()
+            )
+            .await,
+            Ok(Event {
+                id: "6d470410-5e51-40d1-bd13-0bb6a99de95e".into(),
+                name: "Company year celebration".into(),
+                begin: "2025-11-29T18:30Z".into(),
+                end: "2025-11-29T23:59Z".into(),
+                category: EventCategory::Party,
+                frequency: None,
+                weekend_repeat: None,
+                user: "a6edc906-2f9f-5fb2-a373-efac406f0ef2".into(),
+                created_at: "2025-02-05T22:49Z".into(),
+                updated_at: "2025-02-05T22:49Z".into(),
+            })
+        );
+    }
+}
