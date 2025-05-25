78bc6d73facda20e5157a4905d9b9b0d11832b59
joao-arthur <joao.lothamer@gmail.com>
2025-05-25 10:21:53 -0300

    fix(backend): create and delete user integration test

--- backend/tests/integration.rs
+++ backend/tests/integration.rs
@@ -1,46 +1,66 @@
 use axum::http::StatusCode;
 use reqwest::{
-    Client,
-    header::{ACCEPT_LANGUAGE, HeaderMap, HeaderValue},
+    header::{HeaderMap, HeaderValue, ACCEPT_LANGUAGE, AUTHORIZATION, CONTENT_TYPE}, Client
 };
 use serde::Deserialize;
 use serde_json::json;
 use tokio;
-
-use backend::server::start_server;
 
 #[derive(Deserialize, Debug, PartialEq)]
 struct ErrorBody {
     message: String,
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, PartialEq, Deserialize)]
 pub struct EncodedSession {
     pub token: String,
 }
 
-#[tokio::test]
-async fn integration_tests() {
-    let _srv = tokio::spawn(async { start_server().await });
-    tokio::time::sleep(std::time::Duration::from_millis(5000)).await;
-    let session = test_sign_up_ok().await;
-}
-
-async fn test_sign_up_ok() -> EncodedSession {
+fn build_sessionless_client() -> Client {
     let mut headers = HeaderMap::new();
     headers.insert(ACCEPT_LANGUAGE, HeaderValue::from_static("pt-BR"));
-    let client = Client::builder().default_headers(headers.clone()).build().unwrap();
+    headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
+    Client::builder().default_headers(headers.clone()).build().unwrap()
+}
+
+fn build_sessionful_client(token: &str) -> Client {
+    let bearer_token = "Bearer ".to_string() + token;
+    let mut headers = HeaderMap::new();
+    headers.insert(ACCEPT_LANGUAGE, HeaderValue::from_static("pt-BR"));
+    headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
+    headers.insert(AUTHORIZATION, HeaderValue::from_str(&bearer_token).unwrap());
+    Client::builder().default_headers(headers.clone()).build().unwrap()
+}
+
+
+#[tokio::test]
+async fn integration_tests() {
+    let sessionless_client = build_sessionless_client();
+
+    let token = test_user_sign_up(&sessionless_client).await;
+
+    let sessionful_client = build_sessionful_client(&token);
+
+    test_user_delete(&sessionful_client).await;
+}
+
+async fn test_user_sign_up(client: &Client) -> String {
     let req_body = json!({
-        "email": "olivia@gmail.com",
         "first_name": "Olivia Isabel Rodrigo",
+        "email": "olivia@gmail.com",
         "birthdate": "2003-02-20",
         "username": "rod:)",
         "password": "oL1v1@",
     });
-    let res = client.post("http://127.0.0.1:8000/user").json(&req_body).send().await.unwrap();
+    let res = client.post("http://localhost:8000/user").json(&req_body).send().await.unwrap();
     let status = res.status();
     let body = res.json::<EncodedSession>().await.unwrap();
     assert_eq!(status, StatusCode::OK);
 
-    body
+    body.token
+}
+
+async fn test_user_delete(client: &Client) {
+    let res = client.delete("http://localhost:8000/user").send().await.unwrap();
+    assert_eq!(res.status(), StatusCode::NO_CONTENT);
 }
