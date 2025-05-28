ed745a8f1716059443f0dfb237070a05f217b0dc
joao-arthur <joao.lothamer@gmail.com>
2025-05-28 07:37:29 -0300

    feat(backend): create app_event created_at

---
+++ backend/src/migration/m20250528_01_create_app_event_created_at_column.rs
@@ -0,0 +1,20 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let db = manager.get_connection();
+        db.execute_unprepared("ALTER TABLE app_event ADD COLUMN created_at TIMESTAMPTZ NOT NULL")
+            .await?;
+        Ok(())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let db = manager.get_connection();
+        db.execute_unprepared("ALTER TABLE app_event DROP COLUMN created_at").await?;
+        Ok(())
+    }
+}
--- backend/src/migration/mod.rs
+++ backend/src/migration/mod.rs
@@ -13,6 +13,7 @@ mod m20250518_10_create_app_user_first_name_column;
 mod m20250518_11_create_app_user_birthdate_column;
 mod m20250526_01_create_app_event_table;
 mod m20250527_01_create_app_event_primary_key;
+mod m20250528_01_create_app_event_created_at_column;
 
 pub struct Migrator;
 
@@ -33,6 +34,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20250518_11_create_app_user_birthdate_column::Migration),
             Box::new(m20250526_01_create_app_event_table::Migration),
             Box::new(m20250527_01_create_app_event_primary_key::Migration),
+            Box::new(m20250528_01_create_app_event_created_at_column::Migration),
         ]
     }
 }
